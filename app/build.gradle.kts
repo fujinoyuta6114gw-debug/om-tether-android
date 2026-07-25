7a91ba37616f8800f61b0bdbefee3cc0b391a0a9
fujinoyuta6114gw-debug <fujinoyuta6114.gw@gmail.com>
2026-07-26 07:24:55 +0900

    Update app/build.gradle.kts for v0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.omtether"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.3.0"
+        versionCode = 7
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
