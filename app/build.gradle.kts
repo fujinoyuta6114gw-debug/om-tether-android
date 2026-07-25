0a6659cacf9dc6888259356b2eeb544cb7047f9e
fujinoyuta6114gw-debug <fujinoyuta6114.gw@gmail.com>
2026-07-26 06:47:36 +0900

    Implement selectable JPEG or RAW phone saving

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.omtether"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
