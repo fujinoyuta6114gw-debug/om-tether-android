430dfea3d2592eddef8204a1d7211a83f71c8695
fujinoyuta6114gw-debug <fujinoyuta6114.gw@gmail.com>
2026-07-26 10:29:38 +0900

    Bump app version to 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.omtether"
         minSdk = 29
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.3.2"
+        versionCode = 9
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
